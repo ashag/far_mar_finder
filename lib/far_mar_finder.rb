634b7227659cd28c3283c5f8e4f06759956a8249
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-11-11 15:20:38 -0800

    Redo on a couple class files

--- lib/far_mar_finder.rb
+++ lib/far_mar_finder.rb
@@ -1,6 +1,9 @@
 require 'csv'
 require 'time'
 require_relative 'market'
+require_relative 'product'
+require_relative 'sale'
+require_relative 'vendor'
 # ... Require all of the supporting classes
 
 class FarMarFinder
--- lib/market.rb
+++ lib/market.rb
@@ -4,33 +4,35 @@ class Market
   attr_accessor :name, :address #etc
 
   def initialize(array)
-    @name = 
-  end
+    @name
+    end
 
   def self.all_markets
     CSV.read("./support/markets.csv").map do |array|
       Market.new(array)
+    end
   end
+end #end of market
 
-  def ID
-  end
+#   def ID
+#   end
 
-  def name
-  end
+#   def name
+#   end
 
-  def address
-  end
+#   def address
+#   end
 
-  def city
-  end
+#   def city
+#   end
 
-  def county
-  end
+#   def county
+#   end
 
-  def state
-  end
+#   def state
+#   end
 
-  def zip
-  end
+#   def zip
+#   end
     
-end
+# end
