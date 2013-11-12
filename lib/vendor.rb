179a513f9c67f131e3f815564f6ccf41bff9646a
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-11-12 10:50:56 -0800

    Change to vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,15 +1,31 @@
 class Vendor
 
-  def id
-  end
+attr_accessor :id, :name, :No_of_employees, :market_id
 
-  def name
+  def self.id(id)
+      CSV.read("./support/vendors.csv").find do |array|
+        array[0].to_i == id
+      end
   end
-
-  def No_of_employees
+  def self.name(name)
+      CSV.read("./support/markets.csv").find do |array|
+        array[1] == name
+      end
   end
+end  
 
-  def market_id
-  end
+#   def id
+#   end
 
-end
+#   def name
+#   end
+
+#   def No_of_employees
+#   end
+
+#   def market_id
+#   end
+
+#end
+
+
