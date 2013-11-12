967417ad7fcfb2bd9286ebe3f09101ed076fbf2f
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-11-12 13:58:36 -0800

    Changes to product, vendor, n sale

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,37 +1,50 @@
 class Vendor
 
-attr_accessor :id, :name, :No_of_employees, :market_id
-
-  
-  def self.all
-    CSV.read("./support/vendors.csv").map do |array|
-      Vendors.new(array)
+  attr_accessor :id, :name, :no_of_employees, :market_id
+   
+    def initialize(array)
+      @id = array[0]
+      @name = array[1]
+      @no_of_employees = array[2]
+      @market_id = array[3]
     end
-  end
-  def self.id(id)
-      CSV.read("./support/vendors.csv").find do |array|
-        array[0].to_i == id
+
+    def self.all
+      CSV.read("./support/vendors.csv").map do |array|
+        Vendor.new(array)
       end
-  end
-  def self.name(name)
-      CSV.read("./support/markets.csv").find do |array|
-        array[1] == name
+    end
+
+    def self.find(search)
+      all.find do |vendor|
+        vendor.inspect do |x|
+        x = search.to_s
       end
-  end
-end  
+    end 
 
-#   def id
-#   end
+    def id(id)
+        all.find do |vendor|
+          vendor.id.to_i == id
+        end
+      end
+    end
 
-#   def name
-#   end
+    def name(name)
+      all.select do |vendor|
+        vendor.name == name
+      end
+    end
 
-#   def No_of_employees
-#   end
+    def No_of_employees(empl)
+      all.select do |vendor|
+        vendor.no_of_employees.to_i == no_of_employees
+      end
+    end
 
-#   def market_id
-#   end
 
-#end
+    def market_id(m_id)
+    end
+  end  
+
 
 
