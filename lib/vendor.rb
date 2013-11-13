2a2c2262f81a74e876b58eb3c7220adc2bda35fa
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-11-12 22:47:07 -0800

    Rollback on Vendor, was overwritten by push

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,9 +1,8 @@
 class Vendor
 
-  attr_accessor :vendor_id, :name, :no_of_employees, :market_id
+  attr_accessor :id, :name, :no_of_employees, :market_id
    
     def initialize(array)
-      @vendor_id = array[0]
       @id = array[0].to_i
       @name = array[1]
       @no_of_employees = array[2].to_i
@@ -18,19 +17,6 @@ class Vendor
 
     def self.find(id)
       all.find do |vendor|
-        vendor.vendor_id.to_i == search.to_i
-      end
-    end 
-
-    def self.find_by_names(name)
-        all.find do |names|
-          names.name == name
-        end
-    end
-
-    def self.find_all_by_market_id(market_num)
-      all.select do |market|
-        market.market_id.to_i == market_num.to_i
         vendor.id == id.to_i
       end
     end 
@@ -64,6 +50,29 @@ class Vendor
     end
   end  
 
+  #   def id(id)
+  #       all.find do |vendor|
+  #         vendor.id.to_i == id
+  #       end
+  #     end
+  #   end
+
+  #   def name(name)
+  #     all.select do |vendor|
+  #       vendor.name == name
+  #     end
+  #   end
+
+  #   def No_of_employees(empl)
+  #     all.select do |vendor|
+  #       vendor.no_of_employees.to_i == no_of_employees
+  #     end
+  #   end
+
+
+  #   def market_id(m_id)
+  #   end
+  # end  
 
 
 
